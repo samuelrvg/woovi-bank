e5a8004f9721534bffedbceecefc83b7c78c8fae
Samuel Vaz <samuel.rvg@gmail.com>
2024-05-30 12:26:24 -0300

    #1 validate token with use jwt-decode

--- front/src/utils/auth.ts
+++ front/src/utils/auth.ts
@@ -1,4 +1,29 @@
+import { jwtDecode } from 'jwt-decode';
+
+interface DecodedToken {
+  exp: number; // Expiration time in seconds since the epoch
+}
+
 export const isAuthenticated = (): boolean => {
   const token = localStorage.getItem('token');
-  return !!token;
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decodedToken: DecodedToken = jwtDecode<DecodedToken>(token);
+    const currentTime = Date.now() / 1000;
+
+    // Verificar se o token está expirado
+    if (decodedToken.exp < currentTime) {
+      localStorage.removeItem('token');
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error('Invalid token:', error);
+    return false;
+  }
 };
