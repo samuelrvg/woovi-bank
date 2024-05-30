7940c839da2795431fe1446e1cec0cc1f66cd0b1
Samuel Vaz <samuel.rvg@gmail.com>
2024-05-30 00:25:33 -0300

    Configuração inicial para conectar no graphql

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient';
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
   </React.StrictMode>,
 )
